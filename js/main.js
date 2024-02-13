0501311cbf2ecf5545c61affbe3e1ed8f67a3ebe
alivka-oko <alina.okotnikova2002@gmail.com>
2024-02-14 01:25:31 +0300

    scroll + start burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    
+    $('.menu a[href^="#"]').click(function () {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800)
+        $('nav a[href^="#"]').removeClass('active');
+        $(this).addClass('active');
+        return false;
+    })
+    // Модальное меню
+    // $('.burger__menu').click(function () {
+    //     $('.menu__mobile').toggle(500);
+    // })
 })
